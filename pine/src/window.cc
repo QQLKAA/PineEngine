4b920d54625fc3fbe572da7edceeeffc80dd0f23
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2020-10-13 13:33:32 +0200

    Add logging to window class

--- pine/src/window.cc
+++ pine/src/window.cc
@@ -4,13 +4,22 @@ namespace Pine {
 
 Window::Window(
     bool& error, int width, int height, const std::string_view title) {
+  PINE_INFO("Creating window with size ({}, {}) and title \"{}\"",
+            width, height, title);
+  
   if (glfwInit() != GLFW_TRUE) {
+    const char* msg;
+    glfwGetError(&msg);
+    PINE_ERROR("Failed to initialize GLFW: {}", msg);
     error = true;
     return;
   }
 
   _handle = glfwCreateWindow(width, height, title.data(), nullptr, nullptr);
   if (!_handle) {
+    const char* msg;
+    glfwGetError(&msg);
+    PINE_ERROR("Failed to create window: {}", msg);
     error = true;
     return;
   }
@@ -19,6 +28,8 @@ Window::Window(
 }  
 
 Window::~Window() {
+  PINE_TRACE("Destroying window");
+
   if (_handle != nullptr) {
     glfwDestroyWindow(_handle);
     _handle = nullptr;
